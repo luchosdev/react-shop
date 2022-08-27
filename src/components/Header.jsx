5a86b72b39995bdfc1267aec607fcf7302a854b4
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-27 11:35:00 -0500

    add new files, add structure with API

---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useContext } from 'react';
+import '@styles/Header.scss';
+import Menu from '@components/Menu';
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import AppContext from '../context/AppContext';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
+
+const Header = () => {
+	const [toggle, setToggle] = useState(false);
+	const { state } = useContext(AppContext);
+
+	const handleToggle = () => {
+		setToggle(!toggle);
+	}
+
+	return (
+		<nav>
+			<img src={menu} alt="menu" className="menu" />
+			<div className="navbar-left">
+				<img src={logo} alt="logo" className="nav-logo" />
+				<ul>
+					<li>
+						<a href="/">All</a>
+					</li>
+					<li>
+						<a href="/">Clothes</a>
+					</li>
+					<li>
+						<a href="/">Electronics</a>
+					</li>
+					<li>
+						<a href="/">Furnitures</a>
+					</li>
+					<li>
+						<a href="/">Toys</a>
+					</li>
+					<li>
+						<a href="/">Others</a>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-right">
+				<ul>
+					<li className="navbar-email" onClick={handleToggle}>
+						platzi@example.com
+					</li>
+					<li className="navbar-shopping-cart">
+						<img src={shoppingCart} alt="shopping cart" />
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+					</li>
+				</ul>
+			</div>
+			{toggle && <Menu />}
+		</nav>
+	);
+}
+
+export default Header;
---
+++ src/containers/MyOrder.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import OrderItem from '@components/OrderItem';
+import '@styles/MyOrder.scss';
+import arrow from '@icons/flechita.svg';
+
+const MyOrder = () => {
+	return (
+		<aside className="MyOrder">
+			<div className="title-container">
+				<img src={arrow} alt="arrow" />
+				<p className="title">My order</p>
+			</div>
+			<div className="my-order-content">
+				<OrderItem />
+				<div className="order">
+					<p>
+						<span>Total</span>
+					</p>
+					<p>$560.00</p>
+				</div>
+				<button className="primary-button">
+					Checkout
+				</button>
+			</div>
+		</aside>
+	);
+}
+
+export default MyOrder;
