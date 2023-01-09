9e567600c02c9784dc5325feff9ecb699e582e0d
Luis Sequera <luissequeracontreras@gmail.com>
2023-01-09 17:14:52 -0500

    last changes

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -5,37 +5,39 @@ import '@styles/MyOrder.scss';
 import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
-	const { state, toggleOrder } = useContext(AppContext);
+    const { state, toggleOrder } = useContext(AppContext);
 
-	const sumTotal = () => {
-		const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
-		const sum = state.cart.reduce(reducer, 0);
-		return sum;
-	}
+    const sumTotal = () => {
+        const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+        const sum = state.cart.reduce(reducer, 0);
+        return sum;
+    };
 
-	return (
-		<aside className="MyOrder">
-			<div className="title-container">
-				<img className="more-clickable-area pointer"
-				src={arrow} alt="arrow" onClick={()=> toggleOrder()} />
-				<p className="title">My order</p>
-			</div>
-			<div className="my-order-content">
-				{state.cart.map(product => (
-					<OrderItem product={product} key={`orderItem-${product.id}`} />
-				))}
-				<div className="order">
-					<p>
-						<span>Total</span>
-					</p>
-					<p>${sumTotal()}</p>
-				</div>
-				<button className="primary-button">
-					Checkout
-				</button>
-			</div>
-		</aside>
-	);
-}
+    return (
+        <aside className="MyOrder">
+            <div className="title-container">
+                <img
+                    className="more-clickable-area pointer"
+                    src={arrow}
+                    alt="arrow"
+                    onClick={() => toggleOrder()}
+                />
+                <p className="title">My order</p>
+            </div>
+            <div className="my-order-content">
+                {state.cart.map((product) => (
+                    <OrderItem product={product} key={`orderItem-${product.id}`} />
+                ))}
+                <div className="order">
+                    <p>
+                        <span>Total</span>
+                    </p>
+                    <p>${sumTotal()}</p>
+                </div>
+                <button className="primary-button">Checkout</button>
+            </div>
+        </aside>
+    );
+};
 
 export default MyOrder;
